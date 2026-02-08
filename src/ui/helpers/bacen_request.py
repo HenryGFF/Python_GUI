c49b7a004f330bb75ae7384f0059a7a586d5b591
Henry GFF <hgfariasfiore@gmail.com>
2026-02-08 00:03:43 -0300

    adicionando um bagulho dahora aí

---
+++ src/ui/helpers/bacen_request.py
@@ -0,0 +1,41 @@
+import datetime
+from datetime import timedelta
+
+import requests
+
+def get_ptax(data):
+    data_inicial = data - timedelta(days=14)
+
+    dt = data.strftime('%d/%m/%Y')
+    di = data_inicial.strftime('%d/%m/%Y')
+
+    url = (
+    "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados"
+    f"?formato=json&dataInicial={di}&dataFinal={dt}"
+    )
+
+    r = requests.get(url, timeout=10)
+
+    print("Status:", r.status_code)
+
+    dados = r.json()
+    return dados
+
+def get_ptax_single_date(date):
+    dados = get_ptax(date)
+    return dados[-1] #último dia útil
+
+def get_ptax_period(start_date, end_date):
+    ptax_list = []
+    delta = end_date - start_date
+
+    for i in range(delta.days+1):
+        dia = start_date + timedelta(days=i)
+        dados = get_ptax(dia)
+        ptax_list.append(dados[-1])
+
+    print(ptax_list)
+
+d1 = datetime.date(2026, 2, 7)
+d2 = datetime.date(2026, 1, 31)
+get_ptax_period(d2, d1)
